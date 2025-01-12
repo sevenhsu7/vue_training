b14ca6347cc6921f3f53909d8da65d7c1ba423e4
Seven Hsu <xushengwen@echo.tech>
2025-01-12 17:08:01 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,22 @@
-git push -f git@github.com:sevenhsu7/vue_training.git master:gh-pages 
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 构建
+npm run build
+
+# 进入构建输出目录
+cd dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你想要部署到 https://sevenhsu7.github.io/vue_training
+git push -f git@github.com:sevenhsu7/vue_training.git main:gh-pages
+
+cd - 
